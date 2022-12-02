27d92e1114277f1a748b32ac12e2967e5bdb8197
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-12-03 02:49:26 +0900

    :sparkles: feat : 게임방 목록 불러오기

--- src/pages/wait.jsx
+++ src/pages/wait.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import RoomCard from '../components/RoomCard';
+import { firestore } from '../firebase/firestore';
 
 export default function Wait() {
-  return <>대기방</>;
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    firestore.collection('rooms').onSnapshot((d) => {
+      if (d.docs) {
+        setRooms(d.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+      }
+    });
+  }, []);
+
+  return (
+    <Container>
+      <h1>대기방</h1>
+      {rooms.map((room) => (
+        <RoomCard key={room.id} room={room} />
+      ))}
+    </Container>
+  );
 }
 
