1e0cbae241bfbdd9d9aada9a249974356a83bcde
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-12-03 03:39:37 +0900

    :sparkles: feat : 게임 페이지 개발 중

--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import LinkIcon from '@mui/icons-material/Link';
+import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import LinkIcon from '@mui/icons-material/Link';
+import React, { useState } from 'react';
 
 import Header from '../components/Header';
+import { firestore } from '../firebase/firestore';
+import { getDocumentId } from '../utils/util';
 
 const ContainerStyles = {
   width: '100%',
@@ -22,6 +24,35 @@ const TitleStyles = {
 };
 
 export default function Create() {
+  const roomId = getDocumentId();
+  const [name, setName] = useState('');
+  const [peopleCount, setPeopleCount] = useState(0);
+
+  const create = () => {
+    firestore
+      .collection('rooms')
+      .doc(roomId)
+      .set({
+        name,
+        peopleCount: Number(peopleCount),
+        dateCreated: new Date().toISOString(),
+        users: [],
+      })
+      .then(() => {
+        // TODO
+        // 대기방으로 가기
+      })
+      .catch((err) => {
+        console.log('err', err);
+        alert('방 생성에 문제가 생겼습니다');
+      });
+  };
+
+  const linkCopy = () => {
+    const url = process.env.REACT_APP_DEFAULT_URL + `?roomId=${roomId}`;
+    navigator.clipboard.writeText(url);
+  };
+
   return (
     <Container maxWidth="sm" sx={ContainerStyles}>
       <Header />
@@ -32,21 +63,34 @@ export default function Create() {
           </Typography>
           <div>
             <Typography variant="subtitle2">방 이름</Typography>
-            <TextField id="title" variant="standard" fullWidth />
+            <TextField
+              id="title"
+              variant="standard"
+              fullWidth
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div>
             <Typography variant="subtitle2">최대 인원 수</Typography>
-            <TextField id="limit" variant="standard" fullWidth />
+            <TextField
+              id="limit"
+              variant="standard"
+              fullWidth
+              onChange={(e) => setPeopleCount(e.target.value)}
+            />
           </div>
           <Stack direction="row" alignItems="center" spacing={2}>
             <Typography variant="subtitle2">친구와 함께 하기</Typography>
-            <Button startIcon={<LinkIcon />}>링크 복사하기</Button>
+            <Button startIcon={<LinkIcon />} onClick={linkCopy}>
+              링크 복사하기
+            </Button>
           </Stack>
         </Stack>
         <Button
           variant="contained"
           fullWidth
           sx={{ padding: '1rem 0', fontSize: '1rem', borderRadius: '1rem' }}
+          onClick={create}
         >
           생성하기
         </Button>
--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import { Button } from '@mui/material';
+import { Box, Container } from '@mui/system';
+import React, { useEffect, useState } from 'react';
+import { firestore } from '../firebase/firestore';
 
 export default function Game() {
-  return <>게임</>;
+  const [room, setRoom] = useState();
+
+  useEffect(() => {
+    const roomId = 'vtzcZRnFKNyiGMMgrtOn';
+    firestore
+      .collection('rooms')
+      .doc(roomId)
+      .onSnapshot((d) => {
+        setRoom({ id: d.id, ...d.data() });
+      });
+  }, []);
+
+  return (
+    <Container>
+      <Box style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+        <Button style={{ width: '33%', fontSize: '35px' }}>✊</Button>
+        <Button style={{ width: '33%', fontSize: '35px' }}>✌️</Button>
+        <Button style={{ width: '33%', fontSize: '35px' }}>🖐</Button>
+      </Box>
+    </Container>
+  );
 }
 
