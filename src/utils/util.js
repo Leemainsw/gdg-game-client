d48d7265953c3f00095810d3dc23d69ecec582a8
Lee_yam <48716775+Leemainsw@users.noreply.github.com>
2023-08-04 15:42:00 +0900

    refactor: utils 함수 const 형식 맞춤

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,4 +1,4 @@
-function getDocumentId () {
+const getDocumentId = () => {
   const list = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
   let res = '';
   for (let i = 0; i < 20; i += 1) {
